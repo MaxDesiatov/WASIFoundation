a1e5e5e29cbc3f8172c659a642584cab7b9173a3
Max Desiatov <max@desiatov.com>
2020-05-06 15:23:51 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,10 +10,6 @@ let package = Package(
     // Products define the executables and libraries produced by a package,
     // and make them visible to other packages.
     .library(
-      name: "Foundation",
-      targets: ["WASIFoundation"]
-    ),
-    .library(
       name: "WASIFoundation",
       targets: ["WASIFoundation"]
     ),
